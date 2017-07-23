b2a0f39e3b259065441a25c9935487eefe35b1e4
Rolf Koenders <post@madebyrolf.nl>
2017-07-23 23:16:33 +0200

    First steps scraping Github trending

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+const GITHUB_TRENDING_URL = 'https://github.com/trending';
+
+request(GITHUB_TRENDING_URL, (error, response, html) => {
+	if (!error) {
+		const $ = cheerio.load(html);
+
+		$('.repo-list')
+			.children('li')
+			.each(function(i, el) {
+				console.log($(this).find('h3').text().trim());
+			});
+	}
+});
