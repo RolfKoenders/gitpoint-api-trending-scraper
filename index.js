f889e4157064d488ea5cce11b3ddfffa2b989e47
Rolf Koenders <post@madebyrolf.nl>
2017-07-24 22:43:29 +0200

    Add mongoose model. Save day to mongo

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 'use strict';
 
 const trendingScraper = require('./lib/trending-scraper');
+const TrendingDay = require('./models/TrendingDay');
 
-trendingScraper.scrapeIt()
-    .then((repos) => {
-        console.log(repos);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/gitpoint');
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+    console.error(error);
+});
+
+db.once('open', () => {
+    trendingScraper.scrapeIt()
+        .then((repos) => {
+            console.log(repos);
+            let day = new TrendingDay({
+                repositories: repos
+            });
+
+            day.save((err, day) => {
+                if (err) {
+                    return console.error(err);
+                } else {
+                    console.log('Day inserted');
+                    mongoose.disconnect();
+                }
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+});
