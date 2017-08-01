ed4358562b79a5c54a9968ffd786ff2a0b108139
Rolf Koenders <post@madebyrolf.nl>
2017-08-01 23:28:48 +0200

    Refactored some code. Added HTTP listener.

--- index.js
+++ index.js
@@ -1,77 +1,35 @@
 'use strict';
 
-const config = require('./config');
-const mongoose = require('mongoose');
-const moment = require('moment');
 const CronJob = require('cron').CronJob;
 
-const trendingScraper = require('./lib/trending-scraper');
-const TrendingDay = require('./models/TrendingDay');
+const config = require('./config');
+const logger = require('./utils/logger');
+const mongo = require('./utils/mongo');
 
-const pino = require('pino')({
-    name: config.logger.name,
-    level: config.logger.level
-});
+const scraper = require('./lib/scraper');
+const httpServer = require('./lib/http-server');
 
-mongoose.Promise = global.Promise;
-const mongodbUri = `mongodb://${config.mongo.host}:${config.mongo.port}/${config.mongo.database}`;
-mongoose.connect(mongodbUri, {
-    useMongoClient: true,
-    reconnectTries: Number.MAX_VALUE
-});
-
-const db = mongoose.connection;
-db.on('error', (error) => {
-    pino.error(error, 'Error while opening connection to Mongo');
-});
-
+// CronJob
 const job = new CronJob(config.cron.pattern, () => {
-    const startOfDay = moment().startOf('day');
-    const endOfDay = moment().endOf('day');
-
-    trendingScraper.scrapeIt()
-        .then((repos) => {
-            pino.trace(repos, 'Scraped repositories.');
-
-            TrendingDay.findOne({
-                date: {
-                    $gte: startOfDay,
-                    $lt: endOfDay
-                }
-            }, (err, trendingDay) => {
-                if (err) {
-                    pino.error(err, 'Error while trying to find in mongo.');
-                    return err;
-                } else {
-                    if (trendingDay === null) {
-                        trendingDay = new TrendingDay({
-                            repositories: repos
-                        });
-                    } else {
-                        trendingDay.repositories = repos;
-                    }
-
-                    trendingDay.save((err, day) => {
-                        if (err) {
-                            pino.error(err, 'Error while saving trendingday to mongo.');
-                            return err;
-                        } else {
-                            pino.info('TrendingDay saved in Mongo.');
-                        }
-                    });
-                }
-            });
-
+    scraper.scrape()
+        .then(() => {
+            logger.trace('Succesfully inserted scraped data');
         })
-        .catch((error) => {
-            pino.error(error, 'Error while scraping repositories from Github.');
+        .catch((err) => {
+            logger.error('Succesfully inserted scraped data');
         });
 }, () => {
     // Function is called when cronjob is stopped.
-    pino.info('Cronjob stopped. If you see this the cronjob probably crashed..');
+    logger.info('Cronjob stopped. If you see this the cronjob probably crashed..');
 }, false, 'Europe/Amsterdam');
 
-db.once('open', () => {
+// Open DB connection and start CronJob!
+mongo.once('open', () => {
     // Start cronjob
     job.start();
+});
+
+// Start listening for HTTP requests
+httpServer.listen(1337, '0.0.0.0', () => {
+    logger.info('HTTP Server is listening on port: 1337');
 });
