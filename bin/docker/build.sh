b7039c50854bee69e8a3510b6de929fb7cbb4d1f
Rolf Koenders <post@madebyrolf.nl>
2017-08-07 21:55:38 +0200

    Fix the travis docker login

--- bin/docker/build.sh
+++ bin/docker/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 DOCKER_REPOSITORY="rolfkoenders/gitpoint-api-trending-scraper"
-DOCKER_TAG="develop"
+DOCKER_TAG="develop"	# default tag
+
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+	$DOCKER_TAG="master"
+fi
+
+echo "Docker login"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 echo "Building $DOCKER_REPOSITORY image with tag $DOCKER_TAG"
-
 docker build -t "$DOCKER_REPOSITORY:$DOCKER_TAG" ./
-docker push "$DOCKER_REPOSITORY:$DOCKER_TAG"
-docker rmi "$DOCKER_REPOSITORY:$DOCKER_TAG"
 
+docker push "$DOCKER_REPOSITORY:$DOCKER_TAG"
 echo "Image: $DOCKER_REPOSITORY:$DOCKER_TAG pushed successfully"
+
